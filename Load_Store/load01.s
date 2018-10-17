4cca6ba8255e9b912f64b16c9ac6a22aea3737c1
Killian168 <odalyki@outlook.com>
2018-10-17 17:48:07 +0100

    Assembly Code for the first week of Computer Systems

---
+++ Load_Store/load01.s
@@ -0,0 +1,33 @@
+/* -- load01.s */
+
+/* -- Data Section */
+.data
+
+/* Ensure variable is 4 bytes aligned */
+.balign 4
+/* Define storage for myvar1 */
+myvar1:
+	/* Contents of myvar1 is just 4 bytes containing value '3; */
+	.word 3
+
+.balign 4
+myvar2:
+	.word 4
+
+/* Code Section */
+.text
+
+/* Ensure code is 4 byte aligned */
+.balign 4
+.gloabl main
+main:
+	ldr r1, addr_of_myvar1
+	ldr r1, [r1]
+	ldr r2, addr_of_myvar2
+	ldr r2, [r2]
+	add r0, r1, r2
+	bx lr
+
+/* Labels */
+addr_of_myvar1 : .word myvar1
+addr_of_myvar2 : .word myvar2
---
+++ first/first.s
@@ -0,0 +1,7 @@
+/* --first.s */
+/* This is my first comment */
+.global main /* 'main' is our entry poiny and must be global */
+
+main:
+	mov r0, #2
+	bx lr
